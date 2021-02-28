c060e459e139b431b89b183c8a0dc07d65dde32b
Lorena Ontiveros <lorena0328o@gmail.com>
2021-02-28 13:53:50 -0600

    added notes into my javascript to show what the code is for and doing I also added the css style sheet script tags to the html file.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const canvas = document.getElementById("gameArea");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("gameArea"); //grabs the canvas
+const ctx = canvas.getContext("2d"); //tells the program what kind of figures you'll be placing
 
+//  creating the size and color for the canvas.
 ctx.fillStyle = "black";
 ctx.fillRect(0,0, canvas.clientWidth, canvas.height);
